6407764e109c7e91cb55a270b68e19da7fcc53fe
Chuanyu Wang <chuanyu.wang@sap.com>
2018-05-11 09:35:11 +0800

    test create game in test console

--- src/js/components/test-console.vue
+++ src/js/components/test-console.vue
@@ -3,8 +3,9 @@
 </style>
 
 <template lang="pug">
-div
-  div {{msg}}
+div.container
+  div(style='width:100%;height:400px') {{result}}
+  button.btn.btn-primary(@click='createGame', type='button') Create
 </template>
 
 <script>
@@ -21,13 +22,56 @@ module.exports = {
   },
   data: function() {
     return {
-      msg: "hello world"
+      result: ""
     };
   },
   components: {},
   computed: {},
   filters: {},
-  methods: {},
+  methods: {
+    createGame: function(e) {
+      var vm = this;
+      var request = $.ajax("/wx/creategame", {
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+          type: "lyingman",
+          player_number: 6,
+          wolf_number: 2,
+          villager_number: 2,
+          wolf_roles: [
+            {
+              name: "白狼王",
+              value: "white",
+              checked: true
+            }
+          ],
+          roles: [
+            {
+              name: "预言家",
+              value: "seer",
+              checked: "true"
+            },
+            {
+              name: "女巫",
+              value: "witch",
+              checked: "true"
+            }
+          ],
+          options: {
+            witch_save_on_first_night: false
+          }
+        }),
+        dataType: "json"
+      });
+      request.fail(function(jqXHR, textStatus, errorThrown) {
+        console.error(jqXHR.responseJSON ? jqXHR.responseJSON : jqXHR.responseText);
+      });
+      request.done(function(data, textStatus, jqXHR) {
+        vm.result = data;
+      });
+    }
+  },
   created: function() {
     //var vm = this;
   },

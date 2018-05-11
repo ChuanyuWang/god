6e07d141b0f7f47263ed51d775339c9e8d78938d
Chuanyu Wang <chuanyu.wang@sap.com>
2018-05-11 09:36:48 +0800

    fix the error without judge

--- src/js/components/test-console.vue
+++ src/js/components/test-console.vue
@@ -60,7 +60,8 @@ module.exports = {
           ],
           options: {
             witch_save_on_first_night: false
-          }
+          },
+          judge: "xxx"
         }),
         dataType: "json"
       });

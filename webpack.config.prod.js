7dc7d51762b607ec928644a157b59673856f6e0d
Chuanyu Wang <chuanyu.wang@sap.com>
2020-09-28 19:42:31 +0800

    add comment in files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js', '.vue', '.json'],
+        //Create aliases to import or require certain modules more easily.
         alias: {
             //vue: 'vue/dist/vue.min.js'
         }

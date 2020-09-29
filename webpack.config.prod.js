1af6c0a8429cb4068d9763a69a464b2c972e7474
Chuanyu Wang <chuanyu.wang@sap.com>
2020-09-29 15:55:39 +0800

    update webpack.config with unique bundle name

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,7 +20,7 @@ module.exports = {
     ],
 
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'public/js'),
         publicPath: '/js/'
     },
